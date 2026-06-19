0f2f46d26b02946d5989ab4b6281e5c6f0b13aaa
simeonkihekapro-cyber <simeonkihekapro@gmail.com>
2026-06-20 00:41:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT_YOUR_SESSON-ID_HERE",
+SESSION_ID: process.env.SESSION_ID || "ZR9EAZpA#zn_0fbLe2lokGzjpao9YYAI7zaRrhZSHQJKonNvh16Q",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 };
